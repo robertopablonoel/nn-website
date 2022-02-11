180574db3b90d08cb7669b3ddac613e2ead764a9
Roberto Noel <rpn241@nyu.edu>
2022-02-11 09:49:42 -0600

    add art engine

---
+++ hashlips_art_engine-main/src/config.js
@@ -0,0 +1,144 @@
+const basePath = process.cwd();
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+
+const network = NETWORK.sol;
+
+// General metadata for Ethereum
+const namePrefix = "Naughty Neighbors";
+const description = "2121 unique neighbors";
+const baseUri = "ipfs://NewUriToReplace";
+
+const solanaMetadata = {
+  symbol: "NN",
+  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://www.youtube.com/c/hashlipsnft",
+  creators: [
+    {
+      address: "null",
+      share: 100,
+    },
+  ],
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 500,
+    colorMatch: true,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Body" },
+      { name: "Head" },
+      { name: "InnerHead" },
+      { name: "Skin" },
+      { name: "Mouth" },
+      { name: "Nose" },
+      { name: "Eyes" },
+      { name: "Volcano" }
+    ],
+  },
+];
+
+const backgroundMap = {
+  "Green": "Greed",
+  "Pink": "Lust",
+  "Purple": "Pride",
+  "Yellow": "Gluttony"
+}
+
+const bodyMap = {
+  "Anger": "Red",
+  "Envy": "Cyan",
+  "Pride": "Purple",
+  "Sloth": "Blue",
+  "Greed": "Green",
+  "Lust": "Pink",
+  "Gluttony": "Yellow"
+}
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 1500,
+  height: 1500,
+  smoothing: false,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+  backgroundMap,
+  bodyMap
+};
